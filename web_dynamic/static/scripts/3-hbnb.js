b0dcca1a3dcaed3d1c09169be2cd73684a4178c9
emmanavarro <elnavarro55@gmail.com>
2020-09-27 18:55:46 -0500

    To fetch places new endpoint was added to the API

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+  $('input[type=checkbox]').css('margin-right', '10px');
+  $('input[type=checkbox]').on('click', function () {
+    var amenId = [];
+    $('input:checked').each(function () {
+      amenId[$(this).attr('data-id')] = $(this).attr('data-name');
+    });
+    $('input:disabled').each(function () {
+      $(amenId).removeData($(this).attr('data-id'));
+    });
+    var list = [];
+    for (var name in amenId) {
+      list.push(amenId[name]);
+    }
+    $('.amenities h4').text(list.join(', '));
+  });
+});
+
+// Request to API
+const url = 'http://127.0.0.1:5001/api/v1/status/';
+$.get(url, function (data) {
+  if (data.status === 'OK') {
+    $('#api_status').addClass('available');
+  } else {
+    $('#api_status').removeClass('available');
+  }
+});
+
+// Fetch places
+$.ajax({
+  method: 'POST',
+  url: 'http://127.0.0.1:5001/api/v1/places_search/',
+  contenType: 'application/json',
+  data: JSON.stringify(place),
+  success: function (result) {
+    $.each(result, (i, place) => {
+      $('section.places').append(`
+      <article>
+        <div class="title_box">
+          <h2>${place.name}</h2>
+          <div class="price_by_night">${place.price_by_night}</div>
+        </div>
+        <div class="information">
+          <div class="max_guest">${place.max_guest} Guests</div>
+          <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+          <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+        </div>
+        <div class="description">${place.description}</div>
+      </article>`);
+    })
+  }
+});

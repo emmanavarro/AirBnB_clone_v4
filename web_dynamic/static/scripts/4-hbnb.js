c8f7d4f2df95fad086436767f58f7dd5ec3fa705
Emma Navarro <55990484+emmanavarro@users.noreply.github.com>
2020-10-05 19:12:56 -0500

    Update 4-hbnb.js

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,83 +1,65 @@
 $(document).ready(function () {
-  const amenId = [];
-  const ameniList = [];
-  $('input[type=checkbox]').css('margin-right', '10px');
-  $('input[type=checkbox]').on('click', function () {
-    $('input:checked').each(function () {
-      amenId[$(this).attr('data-id')] = $(this).attr('data-name');
+  const amenityDict = {};
+  $('.amenities input:checkbox').on('change', function () {
+    $(this).each(function () {
+      const dataID = $(this).attr('data-id');
+      if (this.checked) {
+        // add Amenity ID to amenityDict
+        amenityDict[dataID] = $(this).attr('data-name');
+      } else {
+        // remove Amenity ID from amenityDict
+        delete amenityDict[dataID];
+      }
     });
-    $('input:disabled').each(function () {
-      $(amenId).removeData($(this).attr('data-id'));
-    });
-    for (var name in amenId) {
-      ameniList.push(amenId[name]);
-    }
-    $('.amenities h4').text(ameniList.join(', '));
-  });
-
-  // Request to API
-  const url = 'http://127.0.0.1:5001/api/v1/status/';
-  $.get(url, function (data) {
-    if (data.status === 'OK') {
-      $('#api_status').addClass('available');
+    if (Object.values(amenityDict).length > 0) {
+      $('.amenities h4').text(Object.values(amenityDict).join(', '));
     } else {
-      $('#api_status').removeClass('available');
+      $('.amenities h4').html('&nbsp;');
     }
   });
 
+  // API status
+ 
+  $.get('http://127.0.0.1:5001/api/v1/status/', function (response) {
+    console.log(response.status);
+    if (response.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+  
   // Fetch places
-  const data = {};
+
+  const place = {};
   $.ajax({
-    url: 'http://127.0.0.1:5001/api/v1/places_search',
-    method: 'POST',
-    dataType: 'json',
+    url: 'http://127.0.0.1:5001/api/v1/places_search/',
+    type: 'POST',
+    data: JSON.stringify(place), // Convert to text string json
     contentType: 'application/json',
-    data: JSON.stringify(data),
-    success: function (result) {
-      $.each(result, (i, place) => {
-        $('section.places').append(`
-        <article>
-          <div class="title_box">
-            <h2>${place.name}</h2>
-            <div class="price_by_night">$${place.price_by_night}</div>
-          </div>
-          <div class="information">
-            <div class="max_guest">${place.max_guest} Guests</div>
-            <div class="number_rooms">${place.number_rooms} Bedrooms</div>
-            <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
-          </div>
-          <div class="description">${place.description}</div>
-        </article>`);
+    dataType: 'json',
+    success: function (places) {
+      $.each(places, function (iterator, place) {
+        $('section.places').append(`<ARTICLE><div class="title_box"><h2>${place.name}</h2><div class="price_by_night">${place.price_by_night}</div></div><div class="information"><div class="max_guest">${place.max_guest} Guest</div><div class="number_rooms">${place.number_rooms} Bedroom</div><div class="number_bathrooms">${place.number_bathrooms} Bathroom</div></div><div class="description">${place.description}</div></ARTICLE>`);
       });
     }
   });
 
   // Filter places by Amenity
-  $('.filters button').click(function () {
-    const ameniCheck = {};
-	ameniCheck.ameniList = Object.keys(ameniList);
+  
+  $('button#filterButton').click(function () {
+    $('article').remove(); // clear screen of all spaces data
+    const place = { amenities: Object.keys(amenityDict) }; // amenity id relationated with place
+    console.log(Object.keys(amenityDict).length);
     $.ajax({
-      url: 'http://127.0.0.1:5001/api/v1/places_search',
-      method: 'POST',
-      dataType: 'json',
+      url: 'http://127.0.0.1:5001/api/v1/places_search/',
+      type: 'POST',
+      data: JSON.stringify(place), // Convert to text string json
       contentType: 'application/json',
-      data: JSON.stringify(ameniCheck),
-      success: function (data) {
-        $('section.places').empty();
-        $.each(data, (i, place) => {
-          $('section.places').append(`
-          <article>
-            <div class="title_box">
-              <h2>${place.name}</h2>
-              <div class="price_by_night">$${place.price_by_night}</div>
-            </div>
-            <div class="information">
-              <div class="max_guest">${place.max_guest} Guests</div>
-              <div class="number_rooms">${place.number_rooms} Bedrooms</div>
-              <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
-            </div>
-            <div class="description">${place.description}</div>
-          </article>`);
+      dataType: 'json',
+      success: function (places) {
+        $.each(places, function (iterator, place) {
+          $('section.places').append(`<ARTICLE><div class="title_box"><h2>${place.name}</h2><div class="price_by_night">${place.price_by_night}</div></div><div class="information"><div class="max_guest">${place.max_guest} Guest</div><div class="number_rooms">${place.number_rooms} Bedroom</div><div class="number_bathrooms">${place.number_bathrooms} Bathroom</div></div><div class="description">${place.description}</div></ARTICLE>`);
         });
       }
     });
